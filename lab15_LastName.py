89b5759b21f4db1058a4c7dd5487359e311ba019
AFulle <afuller@csumb.edu>
2016-12-07 11:02:53 -0800

    Update lab15_LastName.py

--- lab15_LastName.py
+++ lab15_LastName.py
@@ -7,3 +7,36 @@
 
 
 #problem 2: Calendar / Days until birthday / Day Constitution Ratified
+def birthCalendar():
+  import calendar
+  y = int(requestString("Enter the year you were born:"))
+  m = int(requestString("Enter the month(in number form) you were born:"))
+  print(calendar.month(y, m))
+
+def birthdays():
+  from datetime import date
+  today = date.today()
+  y = today.year
+  m = int(requestString("Enter the month(in number form) you were born:"))
+  d = int(requestString("Enter the day(in number form) you were born:"))
+  birthday = date(y, m, d)
+  diff = birthday - today
+  print ("%i day(s) until your birthday") % diff.days
+
+def decDate():
+  from datetime import date
+  day = date(1776, 7, 4).weekday()
+  if day == 0:
+    print "The Declaration of Independence was ratified on Monday"
+  if day == 1:
+    print "The Declaration of Independence was ratified on Tuesday"
+  if day == 2:
+    print "The Declaration of Independence was ratified on Wednesday"
+  if day == 3:
+    print "The Declaration of Independence was ratified on Thursday"
+  if day == 4:
+    print "The Declaration of Independence was ratified on Friday"
+  if day == 5:
+    print "The Declaration of Independence was ratified on Saturday"
+  if day == 6:
+    print "The Declaration of Independence was ratified on Sunday"
