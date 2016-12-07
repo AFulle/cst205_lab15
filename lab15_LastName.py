e16eab14a6e416f9cf4ee88aed11daa482609259
AFulle <afuller@csumb.edu>
2016-12-07 11:20:27 -0800

    Update lab15_LastName.py

--- lab15_LastName.py
+++ lab15_LastName.py
@@ -6,13 +6,14 @@
 #problem 1: Craps
 
 
-#problem 2: Calendar / Days until birthday / Day Constitution Ratified
+#problem 2: Creates and prints calendar of user's birthday
 def birthCalendar():
   import calendar
   y = int(requestString("Enter the year you were born:"))
   m = int(requestString("Enter the month(in number form) you were born:"))
   print(calendar.month(y, m))
 
+#problem 2: Subtracts users birthday from current date and prints how many days until their birthday
 def birthdays():
   from datetime import date
   today = date.today()
@@ -23,6 +24,7 @@ def birthdays():
   diff = birthday - today
   print ("%i day(s) until your birthday") % diff.days
 
+#problem 2: Prints the day the Declaration of Independence was ratified
 def decDate():
   from datetime import date
   day = date(1776, 7, 4).weekday()
